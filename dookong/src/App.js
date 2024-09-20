26f7fb2a5c761ee3fd9433764856969ae1b343d9
wnhaoo <wonah.choi@gmail.com>
2024-09-20 23:42:50 +0900

    마이페이지 완성

--- dookong/src/App.js
+++ dookong/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LankPage from './lank/LankPage';
 import PointPage from './point/PointPage';
 import LoginPage from './login/LoginPage';
+import MyPage from './my/MyPage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/my" element={<MyPage />} />
         <Route path="/lank" element={<LankPage />} />
         <Route path="/point" element={<PointPage />} />
         <Route path="/login" element={<LoginPage />} />
