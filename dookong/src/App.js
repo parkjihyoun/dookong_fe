ef390c30be31e5afb8c81e679f76702c03c7881c
parkjihyoun <jihyoun2002@knu.ac.kr>
2024-09-20 23:16:01 +0900

    회원가입 정보 저장

--- dookong/src/App.js
+++ dookong/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LankPage from './lank/LankPage';
 import PointPage from './point/PointPage';
@@ -8,8 +8,22 @@ import ModalLog from './components/ModalLog'; // Modal 컴포넌트 추가
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false); // 로그인 여부 상태
 
+  useEffect(() => {
+    // 로컬 스토리지에서 로그인 상태를 확인
+    const loggedIn = localStorage.getItem('isAuthenticated');
+    if (loggedIn === 'true') {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   const handleLogin = () => {
-    setIsAuthenticated(true); // 로그인 상태 업데이트
+    setIsAuthenticated(true);
+    localStorage.setItem('isAuthenticated', 'true'); // 로그인 상태를 로컬 스토리지에 저장
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem('isAuthenticated'); // 로그아웃 시 로컬 스토리지에서 삭제
   };
 
   return (
@@ -17,18 +31,18 @@ function App() {
       <Routes>
         <Route 
           path="/lank" 
-          element={isAuthenticated ? <LankPage /> : <ModalLog />} 
+          element={isAuthenticated ? <LankPage onLogout={handleLogout} /> : <ModalLog />} 
         />
         <Route 
           path="/point" 
-          element={isAuthenticated ? <PointPage /> : <ModalLog />} 
+          element={isAuthenticated ? <PointPage onLogout={handleLogout} /> : <ModalLog />} 
         />
         <Route 
           path="/login" 
           element={<LoginPage onLogin={handleLogin} />} 
         />
         {/* 로그인 페이지로 이동 */}
-        {!isAuthenticated && <Route path="*" element={<Navigate to="/login" />} />}
+        <Route path="*" element={<Navigate to={isAuthenticated ? '/lank' : '/login'} />} />
       </Routes>
     </Router>
   );
