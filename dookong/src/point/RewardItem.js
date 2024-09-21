5b9f3e9ae7e6252079876002dd0566b954410a5a
parkjihyoun <jihyoun2002@knu.ac.kr>
2024-09-22 04:28:59 +0900

    콩 이미지 추가

--- dookong/src/point/RewardItem.js
+++ dookong/src/point/RewardItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './RewardItem.css';
-
-const RewardItem = ({ imageSrc, groupSrc, title, description }) => {
+import GroupSrc from "../assets/kong1.svg";  
+const RewardItem = ({ imageSrc, title, description }) => {
   return (
     <div className="frame">
       <div className="group-274">
         <div className="group-273">
           <div className="kong-2">
-            
-            <img className="group" src={groupSrc} alt="Group Icon" />
+            <img className="group" src={GroupSrc} alt="Group Icon" />
           </div>
           <div className="title">{title}</div>
         </div>
